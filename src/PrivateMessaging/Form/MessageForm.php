86577afc1f68f5f09562e13848ba1c86bc864900
Stijn Haulotte <stijnhau@gmail.com>
2014-07-18 20:02:00 +0200

    Overiden the form class will add new field to the view

--- src/PrivateMessaging/Form/MessageForm.php
+++ src/PrivateMessaging/Form/MessageForm.php
@@ -8,64 +8,74 @@ use Zend\Form\Element;
 
 class MessageForm extends ProvidesEventsForm
 {
-    protected $options;
+    private $_options;
 
     public function __construct(MultipleReceiversOptionsInterface $options, $name = 'message')
     {
-        $this->options = $options;
+        $this->_options = $options;
         parent::__construct($name);
     }
 
     public function getOptions()
     {
-        return $this->options;
+        return $this->_options;
     }
 
     public function init()
     {
-        $this->add(array(
-            'name' => 'subject',
-            'type' => 'Text',
-            'options' => array(
-                'label' => 'Subject',
-            ),
-            'attributes' => array(
-                'class' => 'form-control'
-            )
-        ));
-
-        $this->add(array(
-            'name' => 'sender_id',
-            'type' => 'Hidden',
-        ));
-
-        $this->add(array(
-            'name' => 'message_text',
-            'type' => 'Textarea',
-            'options' => array(
-                'label' => 'Message Body',
-            ),
-            'attributes' => array(
-                'class' => 'form-control'
-            )
-        ));
-
         $receiver = new Element\Select('receiver_id');
         $receiver->setLabel('Receiver');
-        $receiver->setAttributes(array(
-            'class' => 'form-control',
-            'multiple' => $this->getOptions()->getEnableMultipleReceivers()
-        ));
+        $receiver->setAttributes(
+            array(
+                'class' => 'form-control',
+                'multiple' => $this->getOptions()->getEnableMultipleReceivers()
+            )
+        );
         $this->add($receiver);
         $this->getEventManager()->trigger('init', $this);
-
-        $this->add(array(
-            'name' => 'submit',
-            'type' => 'Submit',
-            'attributes' => array(
-                'class' => 'btn btn-lg btn-primary',
-                "value" => "Send" 
+        
+        $this->add(
+            array(
+                'name' => 'subject',
+                'type' => 'Text',
+                'options' => array(
+                    'label' => 'Subject',
+                ),
+                'attributes' => array(
+                    'class' => 'form-control'
+                )
             )
-        ));
+        );
+
+        $this->add(
+            array(
+                'name' => 'sender_id',
+                'type' => 'Hidden',
+            )
+        );
+        
+        $this->add(
+            array(
+                'name' => 'message_text',
+                'type' => 'Textarea',
+                'options' => array(
+                    'label' => 'Message Body',
+                ),
+                'attributes' => array(
+                    'class' => 'form-control'
+                )
+            )
+        );
+
+        $this->add(
+            array(
+                'name' => 'submit',
+                'type' => 'Submit',
+                'attributes' => array(
+                    'class' => 'btn btn-lg btn-primary',
+                    "value" => "Send" 
+                )
+            )
+        );
     }
 }
