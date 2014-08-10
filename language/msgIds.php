814b6a4af24b8021eb94f0224c02ea0cc0c08c18
Stijn Haulotte <stijnhau@gmail.com>
2014-08-10 20:43:51 +0200

    - Added string to translate

--- language/msgIds.php
+++ language/msgIds.php
@@ -3,3 +3,14 @@
  * Ids for poedit
  * @method translate()
  */
+translate("Compose Message");
+translate("Sent messages");
+translate("Receiver");
+translate("Subject");
+translate("Message Body");
+translate("Send");
+translate("Inbox Messages");
+translate("Starred Messages");
+translate("Important Messages");
+translate("Unread Messages");
+translate("Sent Messages");
